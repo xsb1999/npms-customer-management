39eb9598a8d385480e74f4c9e82f7d38dc631f04
xsb1999 <18810235625@163.com>
2021-07-09 00:36:19 +0800

    完成登录，展示客户管理页面的两个请求

--- src/main/java/com/neu/customermanagement/management/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/neu/customermanagement/management/service/impl/CustomerServiceImpl.java
@@ -38,17 +38,17 @@ public class CustomerServiceImpl extends ServiceImpl<CustomerMapper, Customer> i
         List<EmpInfo> empInfoList = new ArrayList<>();
         List<CusSearchResult> cusSearchResultList = new ArrayList<>();
 
-        // 事业部总经理（展示所有客户经理的客户列表）
+        // 事业部总经理（展示该部门下客户经理的客户列表）
         if (emp_position.equals("10000030")){
-            // 获取所有部门
-            deptInfoList = customerMapper.getDeptInfoByEmpId(null);
-            // 获取所有的客户经理
-            empInfoList = customerMapper.getEmpInfoByEmpIdAndDeptId(null,null);
-            // 获取当前登录人的客户列表（如果是销售经理则返回该部门下的全部客户，若是总经理，则返回全部客户）
-            cusSearchResultList = customerMapper.getCusSearchResult(null,null);
+            // 获取当前登录人所在部门
+            deptInfoList = customerMapper.getDeptInfoByEmpId(emp_id);
+            // 获取该部门下的所有的客户经理
+            empInfoList = customerMapper.getEmpInfoByEmpIdAndDeptId(null,deptInfoList.get(0).getDeptId());
+            // 获取当前登录人的客户列表（事业部总经理，返回该部门下的全部客户）
+            cusSearchResultList = customerMapper.getCusSearchResult(null, deptInfoList.get(0).getDeptId());
             for (CusSearchResult cr : cusSearchResultList) {
-                // 根据部门id获取部门名称
-                cr.setCusSalesDeptName(employeeMapper.getDeptNameById(cr.getCusSalesDeptId()));
+                // 设定部门名称
+                cr.setCusSalesDeptName(deptInfoList.get(0).getDeptName());
                 // 根据客户经理id获取客户经理姓名
                 cr.setCusCustomerManagerName(employeeMapper.getEmpNameById(cr.getCusCustomerManagerId()));
             }
@@ -59,8 +59,8 @@ public class CustomerServiceImpl extends ServiceImpl<CustomerMapper, Customer> i
             deptInfoList = customerMapper.getDeptInfoByEmpId(emp_id);
             // 获取该部门下的所有的客户经理
             empInfoList = customerMapper.getEmpInfoByEmpIdAndDeptId(null,deptInfoList.get(0).getDeptId());
-            // 获取当前登录人的客户列表（如果是销售经理则返回该部门下的全部客户，若是总经理，则返回全部客户）
-            cusSearchResultList = customerMapper.getCusSearchResult(null, deptInfoList.get(0).getDeptId());
+            // 获取当前登录人的客户列表（销售总监，返回自己的全部客户）
+            cusSearchResultList = customerMapper.getCusSearchResult(emp_id, null);
             for (CusSearchResult cr : cusSearchResultList) {
                 // 设定部门名称
                 cr.setCusSalesDeptName(deptInfoList.get(0).getDeptName());
@@ -74,7 +74,7 @@ public class CustomerServiceImpl extends ServiceImpl<CustomerMapper, Customer> i
             deptInfoList = customerMapper.getDeptInfoByEmpId(emp_id);
             // 获取该客户经理的信息
             empInfoList = customerMapper.getEmpInfoByEmpIdAndDeptId(emp_id, null);
-            // 获取当前登录人的客户列表（如果是销售经理则返回该部门下的全部客户，若是总经理，则返回全部客户）
+            // 获取当前登录人的客户列表（客户经理，返回自己的全部客户）
             cusSearchResultList = customerMapper.getCusSearchResult(emp_id, null);
             for (CusSearchResult cr : cusSearchResultList) {
                 // 设定部门名称
@@ -82,6 +82,16 @@ public class CustomerServiceImpl extends ServiceImpl<CustomerMapper, Customer> i
                 // 设定客户经理姓名
                 cr.setCusCustomerManagerName(empInfoList.get(0).getEmpName());
             }
+        }
+
+        // 营销专员、营销副总和高层领导（可以看到所有部门和客户经理）
+        if (emp_position.equals("20000010") || emp_position.equals("20000020") || emp_position.equals("50000000")){
+            // 获取所有部门
+            deptInfoList = customerMapper.getDeptInfoByEmpId(null);
+            // 获取所有的客户经理
+            empInfoList = customerMapper.getEmpInfoByEmpIdAndDeptId(null,null);
+            // 获取当前登录人的客户列表（默认显示为空）
+            cusSearchResultList = new ArrayList<>();
         }
 
         CusManagePageInfo cusManagePageInfo = new CusManagePageInfo();
